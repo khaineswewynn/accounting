2860219fec3483e04a89f8fd5e97b560a015495b
mohtzz94@gmail.com <admin@Rachels-MacBook.local>
2023-12-10 14:32:06 +0630

    added Sales files

---
+++ src/sales.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>Product Sales</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Sales"
+    }
+</script>
+
+<style scoped>
+
+</style>
